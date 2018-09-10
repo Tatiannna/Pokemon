12249ffc3db02902220904d19308633210235747
Tatianna Robinson <19850813+Tatiannna@users.noreply.github.com>
2018-09-10 15:24:32 -0400

    query results on /pokemon.ejs

--- app.js
+++ app.js
@@ -4,27 +4,22 @@ let P = new Pokedex();
 let bodyParser = require("body-parser");
 let app = express();
 
-let find = function(){
-
-};
-
 const PORT = process.env.PORT || 5000;
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({entended: true}));
 
-
-app.get('/', function(req, res){
+// HOME
+app.get('/', function(req, res){	
 	res.render('index.ejs');
 });
 
-// if (query){
-// 	res.render("pokemon.ejs");
-// }
-
-app.get('/pokemon/:name', function(req, res){
-	console.log(req);
-	res.render('pokemon.ejs');
+// POKEMON
+app.get('/pokemon/', function(req, res){
+	var pokemon = req.query.search;
+	console.log(pokemon);
+	res.render("pokemon.ejs", {name: pokemon});
 });
+
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
